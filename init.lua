64dd5d2d1358754e041daec02e7c91c4d0743a0e
Jeremiah Wilson <jewil@pm.me>
2025-03-23 17:50:15 -0400

    Moved options to options.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- TODO: Remove LSP Autocomplete
 -- TODO: Install Leap
 -- TODO: Figure out what autodeletes whitespace on :w and get rid of it
 
@@ -12,61 +11,7 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ OPTIONS ]]
--- :help vim.opt
--- :help option-list
-
--- Line numbers on
-vim.opt.number = true
--- vim.opt.relativenumber = true
-
--- Enable mouse mode
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
-
--- Sync clipboard between OS and Neovim.
--- Schedule the setting after `UiEnter` because it can increase startup-time.
--- Remove this option if you want your OS clipboard to remain independent.
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
-vim.opt.breakindent = true
-vim.opt.undofile = true
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = 'yes'
-
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live as you type
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 35
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
-vim.opt.confirm = true
+require 'options'
 
 -- [[ KEYMAPS ]]
 -- :help vim.keymap.set()
