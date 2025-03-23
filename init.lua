b5114ea71f96338a6d8876e29e085d1178f0a47b
Jeremiah Wilson <jewil@pm.me>
2025-03-23 18:04:21 -0400

    Moved lazy.nvim init to lazy-init.lua

--- init.lua
+++ init.lua
@@ -17,17 +17,7 @@ require 'keymaps'
 
 require 'autocommands'
 
--- [[ INSTALL `lazy.nvim` PLUGIN MANAGER ]]
--- :help lazy.nvim.txt or https://github.com/folke/lazy.nvim
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
-end ---@diagnostic disable-next-line: undefined-field
-vim.opt.rtp:prepend(lazypath)
+require 'lazy-init' -- enable lazy.nvim package manager
 
 -- [[ INSTALL & CONFIGURE PLUGINS ]]
 -- To check the current status of your plugins, run :Lazy
