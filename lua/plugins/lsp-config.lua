8b41a15652ea4759619fc2d2f51f9d3126f5d046
Jeremiah Wilson <jewil@pm.me>
2025-03-24 16:22:33 -0400

    Added LSP basics and telescope UI for code actions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
 
         config = function()
             require('mason-lspconfig').setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "ts_ls" }
             })
         end
     },
@@ -23,6 +23,11 @@ return {
 
             -- need to call this for every lsp we use
             lspconfig.lua_ls.setup({})
+            lspconfig.ts_ls.setup({})
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,7 @@
 return {
+{
   'nvim-telescope/telescope.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
+  dependencies = { 'nvim-lua/plenary.nvim', },
   -- The config function runs when the plugin loads
   config = function()
     local telescope = require 'telescope'
@@ -31,4 +30,23 @@ return {
     --keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
     --keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' })
   end,
+},
+{
+    "nvim-telescope/telescope-ui-select.nvim",
+    -- This is your opts table
+    config = function()
+        require("telescope").setup({
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown {
+                -- even more opts
+              }
+            }
+          }
+        })
+        -- To get ui-select loaded and working with telescope, you need to call
+        -- load_extension, somewhere after setup function:
+        require("telescope").load_extension("ui-select")
+    end
+}
 }
