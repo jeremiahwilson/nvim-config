8278883b06a47ea63773379beba2062d9cea4fb1
Jeremiah Wilson <jewil@pm.me>
2025-03-24 11:48:21 -0400

    Switched telescope binds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,7 @@
--- lua/plugins/telescope.lua
-
--- Since telescope is loaded via lazy.nvim, you don't need to require it explicitly here
--- Your configuration will be applied once lazy.nvim loads telescope
-
--- This is the standard way to configure a plugin with lazy.nvim
 return {
   'nvim-telescope/telescope.nvim',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    -- You can list other dependencies here, like:
-    -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
   },
   -- The config function runs when the plugin loads
   config = function()
@@ -34,14 +26,9 @@ return {
 
     -- Set up keymaps
     local keymap = vim.keymap.set
-    keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
-    keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
-    keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
-    keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' })
+    keymap('n', 'S', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
+    keymap('n', 's', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
+    --keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
+    --keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' })
   end,
-  -- Optional: You can specify lazy-loading if you want
-  -- cmd = "Telescope",
-  -- keys = {
-  --   { "<leader>f", desc = "Telescope" },
-  -- },
 }
