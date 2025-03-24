f80038e2195b925bfc7886d4032ea80a7d78be74
Jeremiah Wilson <jewil@pm.me>
2025-03-24 01:46:09 -0400

    Moved leap to f

--- init.lua
+++ init.lua
@@ -1,12 +1,15 @@
--- TODO: Install Leap
--- TODO: Setup LSP
+-- TODO: Configure telescope from scratch with typecraft video
+-- TODO: Setup LSP probably also in typecraft
 -- TODO: Setup neotree
+-- IDEA: map s to grep via telescope. S file search?
+-- IDEA: shift-J shift-K scroll 25% of visible buffer
+-- so if theres 100 lines showing on my screen and im on line 50, move cursor to line 75
+-- and put line 75 where line 50 was. 
 
 -- [[ GLOBALS ]]
--- Set <space> as the leader key
--- See `:help mapleader`
--- Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
+-- `:help mapleader`
+-- Must happen before plugins are loaded
+vim.g.mapleader = ' ' -- <space>
 vim.g.maplocalleader = ' '
 
 vim.g.have_nerd_font = true
