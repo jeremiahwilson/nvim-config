c0340a70db1c2c9b6ac342efaf018bbd624607fe
Jeremiah Wilson <jewil@pm.me>
2025-03-24 11:22:43 -0400

    Fixed tabs

--- lua/options.lua
+++ lua/options.lua
@@ -52,3 +52,8 @@ vim.opt.scrolloff = 35
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 vim.opt.confirm = true
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
