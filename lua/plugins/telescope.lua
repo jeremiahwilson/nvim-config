90c381b64b8910dd7b7803efb121922ae70c9e51
Jeremiah Wilson <jewil@pm.me>
2025-03-27 13:48:50 -0400

    Removed double setup calls for Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,53 +1,46 @@
 return {
-{
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', },
-  -- The config function runs when the plugin loads
-  config = function()
-    local telescope = require 'telescope'
-
-    telescope.setup {
-      defaults = {
-        -- Your defaults config here
-        respect_gitignore = true,
-        mappings = {
-          i = {
-            -- Insert mode mappings
-          },
-          n = {
-            -- Normal mode mappings
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    -- The config function runs when the plugin loads
+    config = function()
+      local telescope = require('telescope')
+      telescope.setup({
+        defaults = {
+          -- Your defaults config here
+          respect_gitignore = true,
+          file_ignore_patterns = {"package.json", "package%-lock.json", "node_modules"},
+          mappings = {
+            i = {
+              -- Insert mode mappings
+            },
+            n = {
+              -- Normal mode mappings
+            },
           },
         },
-      },
-      extensions = {
-        -- Extensions config here
-      },
-    }
-
-    -- Set up keymaps
-    local keymap = vim.keymap.set
-    keymap('n', 'S', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
-    keymap('n', 's', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
-    --keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
-    --keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' })
-  end,
-},
-{
-    "nvim-telescope/telescope-ui-select.nvim",
-    -- This is your opts table
-    config = function()
-        require("telescope").setup({
-          extensions = {
-            ["ui-select"] = {
-              require("telescope.themes").get_dropdown {
-                -- even more opts
-              }
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+              -- even more opts
             }
           }
-        })
-        -- To get ui-select loaded and working with telescope, you need to call
-        -- load_extension, somewhere after setup function:
-        require("telescope").load_extension("ui-select")
+        }
+      })
+      
+      -- Set up keymaps
+      local keymap = vim.keymap.set
+      keymap('n', 'S', '<cmd>Telescope find_files<cr>', { desc = 'Find files' })
+      keymap('n', 's', '<cmd>Telescope live_grep<cr>', { desc = 'Live grep' })
+      --keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Find buffers' })
+      --keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Help tags' })
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      -- Load the extension AFTER telescope has been set up
+      require("telescope").load_extension("ui-select")
     end
-}
+  }
 }
